a0b8bcd0c5e551c6cca36bdb0d87e154b47989ee
Ardian <ardiaei099@gmail.com>
2022-10-05 11:35:24 +0700

    file_array

---
+++ Arrays_NumericArrays_1.php
@@ -0,0 +1,9 @@
+<?php
+$halalmart[0] = "milk tea";
+$halalmart[1] = "piscok";
+$halalmart[2] =  "Donat Salju"; 
+
+echo "=============DAFTAR BARANG YANG DIJUAL===============\n";
+echo "1.$halalmart[0]\n2.$halalmart[1]\n3.$halalmart[2]\n";
+echo "=====================================================";
+?>
